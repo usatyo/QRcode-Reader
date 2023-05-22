9c52c2085c1e6ebb70876753a97891eca33ae52f
usatyo <yu0424.math8128@gmail.com>
2023-05-23 03:36:16 +0900

    add comment

--- index.js
+++ index.js
@@ -2,34 +2,25 @@ let canvasWidth = 100;
 let canvasHeight = 100;
 let uploadImgSrc;
 
+// 画像を変更したときにその画像を読み取るスクリプト
 function loadLocalImage(e) {
   let canvas = document.getElementById("canvas");
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
-
-  // ファイル情報を取得
   let fileData = e.target.files[0];
-
-  // FileReaderオブジェクトを使ってファイル読み込み
   let reader = new FileReader();
-  // ファイル読み込みに成功したときの処理
   reader.onload = function () {
-    // Canvas上に表示する
     uploadImgSrc = reader.result;
     canvasDraw();
   };
-  // ファイル読み込みを実行
   reader.readAsDataURL(fileData);
 }
 
-// Canvas上に画像を表示する
+// canvas上に画像を表示するスクリプト
 function canvasDraw() {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
-  // canvas内の要素をクリアする
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-  // Canvas上に画像を表示
   let img = new Image();
   img.src = uploadImgSrc;
   img.onload = function () {
@@ -43,16 +34,17 @@ function canvasDraw() {
   };
 }
 
+// QRコードを読み取るスクリプト
 function readQR() {
-  let result = document.getElementById("result")
+  let result = document.getElementById("result");
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
   let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   var code = jsQR(imageData.data, imageData.width, imageData.height);
-  console.log(code)
-  if (!code){
-    result.textContent = "QRコードが存在しません"
-  }else{
-    result.textContent = code.data
+  console.log(code);
+  if (!code) {
+    result.textContent = "QRコードが存在しません";
+  } else {
+    result.textContent = code.data;
   }
 }
